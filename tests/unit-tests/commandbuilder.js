b04abac989ae128c5f81f6bf10db852cfd00351f
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-10-21 03:33:29 +0300

    refactor(tests): change the old out_a/out_v to the new out_ID version

--- tests/unit-tests/commandbuilder.js
+++ tests/unit-tests/commandbuilder.js
@@ -76,7 +76,7 @@ describe("Command builder", () => {
     resetNodes();
     addNode(makeFilter("filter", "V->V"), "filter");
     expect(get(previewCommand).join(" ")).toBe(
-      `ffmpeg -i punch.mp4 -filter_complex "[0:v]filter[out_v]" -map "[out_v]" -map 0:a out.mp4`
+      `ffmpeg -i punch.mp4 -filter_complex "[0:v]filter[out_0]" -map "[out_0]" -map 0:a out.mp4`
     );
   });
 
@@ -84,7 +84,7 @@ describe("Command builder", () => {
     resetNodes();
     addNode(makeFilter("filter", "a->a"), "filter");
     expect(get(previewCommand).join(" ")).toBe(
-      `ffmpeg -i punch.mp4 -filter_complex "[0:a]filter[out_a]" -map "[out_a]" -map 0:v out.mp4`
+      `ffmpeg -i punch.mp4 -filter_complex "[0:a]filter[out_0]" -map "[out_0]" -map 0:v out.mp4`
     );
   });
 
@@ -93,7 +93,7 @@ describe("Command builder", () => {
     addNode(makeFilter("afilter", "a->a"), "filter");
     addNode(makeFilter("vfilter", "v->v"), "filter");
     expect(get(previewCommand).join(" ")).toBe(
-      `ffmpeg -i punch.mp4 -filter_complex "[0:a]afilter[out_a];[0:v]vfilter[out_v]" -map "[out_a]" -map "[out_v]" out.mp4`
+      `ffmpeg -i punch.mp4 -filter_complex "[0:a]afilter[out_1];[0:v]vfilter[out_0]" -map "[out_1]" -map "[out_0]" out.mp4`
     );
   });
 
@@ -103,7 +103,7 @@ describe("Command builder", () => {
     addNode(makeFilter("vfilter2", "v->v"), "filter");
     addNode(makeFilter("vfilter3", "v->v"), "filter");
     expect(get(previewCommand).join(" ")).toBe(
-      `ffmpeg -i punch.mp4 -filter_complex "[0:v]vfilter,vfilter2,vfilter3[out_v]" -map "[out_v]" -map 0:a out.mp4`
+      `ffmpeg -i punch.mp4 -filter_complex "[0:v]vfilter,vfilter2,vfilter3[out_0]" -map "[out_0]" -map 0:a out.mp4`
     );
   });
 
@@ -113,7 +113,7 @@ describe("Command builder", () => {
     addNode(makeFilter("vfilter", "v->v"), "filter");
     addNode(makeFilter("vfilter2", "v->v"), "filter");
     expect(get(previewCommand).join(" ")).toBe(
-      `ffmpeg -i punch.mp4 -filter_complex "[0:v]vfilter[1];[0:a]afilter[out_a];[1]vfilter2[out_v]" -map "[out_a]" -map "[out_v]" out.mp4`
+      `ffmpeg -i punch.mp4 -filter_complex "[0:v]vfilter[1];[0:a]afilter[out_1];[1]vfilter2[out_0]" -map "[out_1]" -map "[out_0]" out.mp4`
     );
   });
 
@@ -121,31 +121,31 @@ describe("Command builder", () => {
     const examples = [
       [
         "crop_trim.json",
-        `ffmpeg -i punch.mp4 -filter_complex "[0:v]crop=out_w=iw/2,trim=start=1.7:duration=0.5[out_v]" -map "[out_v]" out.gif`,
+        `ffmpeg -i punch.mp4 -filter_complex "[0:v]crop=out_w=iw/2,trim=start=1.7:duration=0.5[out_0]" -map "[out_0]" out.gif`,
       ],
       [
         "grid.json",
-        `ffmpeg -i punch.mp4 -i shoe.mp4 -i shoe.mp4 -i punch.mp4 -filter_complex "[1:v][0:v][3:v][2:v]xstack=inputs=4:grid=2x2:shortest=true[out_v]" -map "[out_v]" out.mp4`,
+        `ffmpeg -i punch.mp4 -i shoe.mp4 -i shoe.mp4 -i punch.mp4 -filter_complex "[1:v][0:v][3:v][2:v]xstack=inputs=4:grid=2x2:shortest=true[out_3]" -map "[out_3]" out.mp4`,
       ],
       [
         "scale_overlay.json",
-        `ffmpeg -i punch.mp4 -i shoe.mp4 -filter_complex "[0:v]scale=w=120:h=120:force_original_aspect_ratio=increase[1];[1:v][1]overlay=x=290:y=50[out_v]" -map "[out_v]" out.mp4`,
+        `ffmpeg -i punch.mp4 -i shoe.mp4 -filter_complex "[0:v]scale=w=120:h=120:force_original_aspect_ratio=increase[1];[1:v][1]overlay=x=290:y=50[out_1]" -map "[out_1]" out.mp4`,
       ],
       [
         "smooth_slow.json",
-        `ffmpeg -i punch.mp4 -filter_complex "[0:v]setpts=expr=2*PTS,minterpolate=fps=66[out_v];[0:a]asetpts=expr=2*PTS[out_a]" -map "[out_a]" -map "[out_v]" out.mp4`,
+        `ffmpeg -i punch.mp4 -filter_complex "[0:v]setpts=expr=2*PTS,minterpolate=fps=66[out_0];[0:a]asetpts=expr=2*PTS[out_1]" -map "[out_0]" -map "[out_1]" out.mp4`,
       ],
       [
         "speedup.json",
-        `ffmpeg -i punch.mp4 -filter_complex "[0:v]setpts=expr=0.5*PTS[out_v]" -map "[out_v]" out.mp4`,
+        `ffmpeg -i punch.mp4 -filter_complex "[0:v]setpts=expr=0.5*PTS[out_0]" -map "[out_0]" out.mp4`,
       ],
       [
         "text.json",
-        `ffmpeg -i punch.mp4 -filter_complex "[0:v]drawtext=fontfile=comic.ttf:text=LOL:fontcolor=red:bordercolor=white:boxborderw=3:fontsize=100:x=300:y=150:borderw=5[out_v]" -map "[out_v]" -map 0:a out.mp4`,
+        `ffmpeg -i punch.mp4 -filter_complex "[0:v]drawtext=fontfile=comic.ttf:text=LOL:fontcolor=red:bordercolor=white:boxborderw=3:fontsize=100:x=300:y=150:borderw=5[out_0]" -map "[out_0]" -map 0:a out.mp4`,
       ],
       [
         "xfade.json",
-        `ffmpeg -i punch.mp4 -i shoe.mp4 -filter_complex "[0:v][1:v]xfade=transition=radial:duration=3[out_v]" -map "[out_v]" -map 0:a out.mp4`,
+        `ffmpeg -i punch.mp4 -i shoe.mp4 -filter_complex "[0:v][1:v]xfade=transition=radial:duration=3[out_1]" -map "[out_1]" -map 0:a out.mp4`,
       ],
     ];
 
